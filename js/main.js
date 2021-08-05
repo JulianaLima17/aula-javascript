8f5f5aa447e89184a03d3628d3aacd3ba7c676b4
unknown <juliananascimento11@hotmail.com>
2021-08-05 19:48:20 -0300

    aula javascript

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+var fruta = [{nome: "maça", cor: "vermelha"} , {nome:"uva", cor: "roxa"}];
+console.log(fruta);
+alert(fruta[1].nome);
+
+
+
+
+/*
+var fruta = {nome: "maça", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista =["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop ();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Juliana Lima";
+//var n1 = 5;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+//console.log(nome); 
+//console.log(n1 - idade);
+//console.log(frase.toLowerCase());
+//alert (frase.replace("Japão", "Brasil"));
